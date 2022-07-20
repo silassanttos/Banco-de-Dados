89004e7b982763bf7cb68877b56537fced91c54b
Silas Dos Santos Sousa <69328711+silassanttos@users.noreply.github.com>
2022-07-19 23:30:10 -0300

    Update teste.sql

--- Oracle_/teste.sql
+++ Oracle_/teste.sql
@@ -1,13 +1,22 @@
-set serveroutput on 
+set serveroutput on
+declare
+  v_count number(2);
+  v_salary employees.salary%type;
+  v_new_salary employees.salary%type;
+  EMPID constant number(5) := 102;
 begin
-   update employees set salary = 5000
-   where  department_id = 200;
-   
-   if sql%found then
-       dbms_output.put_line('Updated ' || sql%rowcount || ' employees!');
-       rollback;
-   else
-       dbms_output.put_line('Employee not found!');
-   end if;
-   
-end;   
+  -- get job history count
+  select count(*) into v_count
+  from job_history
+  where employee_id = EMPID;
+  
+  select salary into v_salary
+  from employees
+  where employee_id = EMPID;
+  -- calculate new salary 
+  v_new_salary := v_salary + ( v_salary * (v_count * 10) / 100);
+  
+  dbms_output.put_line('Job History count  : ' || v_count);
+  dbms_output.put_line('Current salary     : ' || v_salary);
+  dbms_output.put_line('New salary         : ' || v_new_salary);
+end;
