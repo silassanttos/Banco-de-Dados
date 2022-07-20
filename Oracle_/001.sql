d8283e886c63559640ad3044e169769295eb06b3
Silas Dos Santos Sousa <69328711+silassanttos@users.noreply.github.com>
2022-07-19 23:27:12 -0300

    Rename 001.sql to Criando_usuario_permissao.sql

--- Oracle_/001.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER silas IDENTIFIED BY "310318"  /*Criando usuário no Oracle*/
-
-grant dba to silas with admin option /*Liberando os privilegios para o usuario*/
---
+++ Oracle_/Criando_usuario_permissao.sql
@@ -0,0 +1,3 @@
+CREATE USER silas IDENTIFIED BY "310318"  /*Criando usuário no Oracle*/
+
+grant dba to silas with admin option /*Liberando os privilegios para o usuario*/
